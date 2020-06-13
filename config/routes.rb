87fc9d590628ad376d64a18a6620b64a60a93550
github username <github email address>
2020-06-12 20:58:37 -0400

    Basic home page - home link to login_path, register_path, users_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  
+  root to: "pages#home", as: "/home"
+  get "/login", to: "pages#login"
+
   resources :locations
   resources :user_hangouts
   resources :hangouts
   resources :users
+  get "/register", to: "users#new"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+Location.destroy_all
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+location_type = ["restaurant", "park", "movie theater", "museum", "roof top", "club", "pub", "church"]
+10.times do 
+  Location.create(location_type: location_type.sample, city: Faker::Address.city, address: Faker::Address.street_address)
+end
+
+marlon = User.create(username: "mrb", first_name: "Marlon", last_name: "Braga", age: "30", bio: "I'm from Brazil.")
+senada = User.create(username: "sk", first_name: "Senada", last_name: "kadric", age: "23", bio: "I'm from Bosnia.")
+# hangout_1 = Hangout.create(nickname: "A walk in the park", activity: "Go for a walk", location_id: 18)
+# date_1 = UserHangout.create(user_id: 1, hangout_id: 1, accepted: false)
+# date_2 = UserHangout.create(user_id: 2, hangout_id: 1, accepted: false)
+
+puts "======="
+puts "SEEDED"
+puts "======="
