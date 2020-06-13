25e1f2ac27ee7807ff998fffac05898f4e1ded49
github username <github email address>
2020-06-12 23:47:05 -0400

    New form for user registration - Need to select multiple interests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,26 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @age_choice = (18..70).to_a
+    @hobbies = ['3D printing', 'Amateur radio', 'Scrapbook', 'Amateur radio', 'Acting', 'Baton twirling', 'Board games', 'Book restoration', 'Cabaret'];
     @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.valid?
+      redirect_to user_path(@user)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :first_name, :last_name, :age, :bio, :interests)
   end
 end
