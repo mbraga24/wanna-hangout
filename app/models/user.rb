a2e4e611529d64f3f661a6e11d7bb2a56eefb156
github username <github email address>
2020-06-15 11:42:21 -0400

    Match testing - User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   has_many :hangouts, through: :user_hangouts
   has_and_belongs_to_many :interests
   has_secure_password
+
+  def user_interests
+    self.interests.map do |interest|
+      interest.name
+    end
+  end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,7 @@ interests = [
   'Amateur radio',
   'Scrapbook',
   'Amateur radio',
-  'Acting',
-  'Baton twirling',
-  'Board games',
-  'Book restoration',
-  'Cabaret',
-  'Calligraphy',
-  'Candle making'
+  'Acting'
 ]
 location_type = ["restaurant", "park", "movie theater", "museum", "roof top", "club", "pub", "church"]
 
@@ -24,10 +18,30 @@ interests.each do |interest|
   Interest.create(name: interest)
 end
 
+book_restoration = Interest.create(name: 'Book restoration')
+cabaret = Interest.create(name: 'Cabaret')
+calligraphy = Interest.create(name: 'Calligraphy')
+candle_making = Interest.create(name: 'Candle making')
+baton_twirling = Interest.create(name: 'Baton twirling')
+board_games = Interest.create(name: 'Board games')
+
 marlon = User.create(username: "mrb", first_name: "Marlon", last_name: "Braga", age: "30", bio: "I'm from Brazil.", password: "1234")
 senada = User.create(username: "sk", first_name: "Senada", last_name: "kadric", age: "23", bio: "I'm from Bosnia.", password: "1234")
+marcos = User.create(username: "mark", first_name: "Marcos", last_name: "Perobelli", age: "58", bio: "Ciao! Io sono Italiano. Voglio fare piu amici qui.", password: "1234")
 
+# Marlon has 2 interests in common with Senada and Marcos has only 1 common interest with Senada
 
+marlon.interests << cabaret
+marlon.interests << calligraphy
+marlon.interests << book_restoration
+
+senada.interests << cabaret
+senada.interests << calligraphy
+senada.interests << candle_making
+
+marcos.interests << candle_making
+marcos.interests << board_games
+marcos.interests << baton_twirling
 
 puts "======="
 puts "SEEDED"
