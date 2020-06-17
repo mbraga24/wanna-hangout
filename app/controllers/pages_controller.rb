ace4aff46ab334ca124bdfb2fcce1ae1defa0005
github username <github email address>
2020-06-17 15:32:38 -0400

    New seed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,4 @@ class PagesController < ApplicationController
   def home
     redirect_to user_path(current_user) if logged_in?
   end
-
-  def new_form
-    respond_to do |format|
-      format.html
-      format.js
-    end
-  end
 end
